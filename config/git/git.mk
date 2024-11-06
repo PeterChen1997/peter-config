de52aa992fe3e227fb27837c63a1094d829bf4f6
Peter Chen <13093023233@163.com>
2024-11-06 11:06:42 +0800

    Create git.mk

---
+++ config/git/git.mk
@@ -0,0 +1,65 @@
+# dev
+d:
+	yarn
+	yarn start
+
+# push
+p:
+	git push
+
+# append to previous pr
+a:
+	git add .
+	git ca --no-edit
+	git push -f
+
+# commit current files with push
+c:
+	git add .
+	git cz
+	$(MAKE) p
+
+cf:
+	git add .
+	git cz --no-edit
+	$(MAKE) p
+
+# commit current files
+cz:
+	git add .
+	git cz
+
+# commit current files without verify
+cw:
+	git add .
+	git cz --no-verify
+
+# commit current files without verify and push
+aw:
+	git add .
+	git ca --no-edit --no-verify
+	git push -f
+
+# switch
+s:
+	git fetch
+	git co $(b)
+	$(MAKE) d
+
+# switch rc
+rc:
+	git co rc
+	git p
+	$(MAKE) d
+
+# switch new branch from rc
+sn:
+	git co rc
+	git pull
+	git co -b $(b)
+	$(MAKE) d
+
+# solve rc conflict
+sr:
+	git fetch
+	git merge origin/rc
